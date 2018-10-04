c5fdf65e51a5d7ed27c70f9f35551fa59ac1fa74
Edwinsyafriana <37358123+Edwinsyafriana@users.noreply.github.com>
2018-10-04 08:35:10 +0700

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'MI2D1731710030'`, async(() => {
+  it(`should have as title 'MI2D1731710081'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('MI2D1731710030');
+    expect(app.title).toEqual('MI2D1731710081');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to MI2D1731710030!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to MI2D1731710081!');
   }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MI2D1731710030';
+    title = 'MI2D1731710030'
 }
